bfb2942d80851db44384f9e0ab6e2145f92ebbec
Yurii <shadowjura82@gmail.com>
2023-04-18 10:43:18 +0200

    task2 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание 1");
         task1();
+        System.out.println("Задание 2");
+        task2();
     }
 
-    public static void task1() {
-        System.out.println("Задание 1");
+    public static String fullNameFunction() {
         String firseName = "Ivan";
         String lastName = "Ivanov";
         String middleName = "Ivanovich";
-        String fullName = "ФИО сотрудника — " + lastName + " " + firseName + " " + middleName + "\n";
+        return lastName + " " + firseName + " " + middleName;
+    }
+
+    public static void task1() {
+        String fullName = "ФИО сотрудника — " + fullNameFunction() + "\n";
         System.out.println(fullName);
+    }
+
+    public static void task2() {
+        String fullName = fullNameFunction().toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName + "\n");
     }
 }
