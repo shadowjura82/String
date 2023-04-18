bf1f61f7926e127c9640e827d9b7addbb6d9939e
Yurii <shadowjura82@gmail.com>
2023-04-18 10:25:03 +0200

    task1 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        System.out.println("Задание 1");
+        String firseName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = "ФИО сотрудника — " + lastName + " " + firseName + " " + middleName + "\n";
+        System.out.println(fullName);
     }
 }
